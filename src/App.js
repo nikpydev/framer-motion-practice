6cfec9ca52966c630b288b941fe37643773384bf
Nikhil Choudhary <nikchoud1@publicisgroupe.net>
2021-06-23 12:57:08 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,58 @@
+import { AnimatePresence } from 'framer-motion';
+import React, { useState } from 'react';
+import { Route, Switch, useLocation } from 'react-router-dom';
+import Base from './components/Base';
+import Header from './components/Header';
+import Home from './components/Home';
+import Modal from './components/Modal';
+import Order from './components/Order';
+import Toppings from './components/Toppings';
+
+function App() {
+  const location = useLocation();
+  const [pizza, setPizza] = useState({ base: '', toppings: [] });
+  const [showModal, setShowModal] = useState(false);
+
+  const closeModal = () => {
+    setShowModal(false);
+  };
+
+  const addBase = (base) => {
+    setPizza({ ...pizza, base });
+  };
+
+  const addTopping = (topping) => {
+    let newToppings;
+    if (!pizza.toppings.includes(topping)) {
+      newToppings = [...pizza.toppings, topping];
+    } else {
+      newToppings = pizza.toppings.filter((item) => item !== topping);
+    }
+    setPizza({ ...pizza, toppings: newToppings });
+  };
+
+  return (
+    <>
+      <Header />
+      <Modal showModal={showModal} setShowModal={setShowModal} />
+      <AnimatePresence exitBeforeEnter onExitComplete={closeModal}>
+        <Switch location={location} key={location.key}>
+          <Route path="/base">
+            <Base addBase={addBase} pizza={pizza} />
+          </Route>
+          <Route path="/toppings">
+            <Toppings addTopping={addTopping} pizza={pizza} />
+          </Route>
+          <Route path="/order">
+            <Order pizza={pizza} setShowModal={setShowModal} />
+          </Route>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
+      </AnimatePresence>
+    </>
+  );
+}
+
+export default App;
